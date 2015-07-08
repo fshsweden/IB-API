15ed420091c5b6bbe3c5aa97b83888fdcf7c1ca6
Peter Andersson <fshsweden@hotmail.com>
2015-07-08 19:11:36 +0200

    first commit

---
+++ src/ib/IApiEnum.java
@@ -0,0 +1,9 @@
+package ib;
+/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+
+
+public interface IApiEnum {
+	String getApiString();
+}
